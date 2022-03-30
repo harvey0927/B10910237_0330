bcdeb001adaf812d8fe4764665ff75915d193b48
harvey0927 <harvey.0927.000@gmail.com>
2022-03-30 14:55:05 +0800

    0330_3.0.1
    p3

---
+++ app/src/main/java/com/example/b10910237_0330/p3.kt
@@ -0,0 +1,11 @@
+package com.example.b10910237_0330
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class p3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p3)
+    }
+}
