38b4af9b4d766b4655d219c0b1b012c3ce150645
harvey0927 <harvey.0927.000@gmail.com>
2022-03-30 13:58:54 +0800

    0330_1
    Hello World!

---
+++ app/src/main/java/com/example/b10910237_0330/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.b10910237_0330
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var v1:TextView=findViewById<TextView>(R.id.textView)
+        v1.text="Hello World!"
+
+
+    }
+}
