f0d0667c6af160a91e76416dca210c5f34811dec
harvey0927 <harvey.0927.000@gmail.com>
2022-03-30 14:08:34 +0800

    0330_2
    butten

--- app/src/main/java/com/example/b10910237_0330/MainActivity.kt
+++ app/src/main/java/com/example/b10910237_0330/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.b10910237_0330
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         var v1:TextView=findViewById<TextView>(R.id.textView)
         v1.text="Hello World!"
+        var b1:Button=findViewById(R.id.button)
 
 
     }
