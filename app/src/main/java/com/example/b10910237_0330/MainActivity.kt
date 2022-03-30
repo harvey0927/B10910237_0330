0d41e4c661a840f3655485cb818aac78fcad4e23
harvey0927 <harvey.0927.000@gmail.com>
2022-03-30 14:19:57 +0800

    0330_3
    IMG

--- app/src/main/java/com/example/b10910237_0330/MainActivity.kt
+++ app/src/main/java/com/example/b10910237_0330/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.b10910237_0330
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         var v1:TextView=findViewById<TextView>(R.id.textView)
         v1.text="Hello World!"
-        var b1:Button=findViewById(R.id.button)
+        var b1:Button=findViewById<Button>(R.id.button)
+        var img1: ImageView=findViewById<ImageView>(R.id.imageView)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            img1.visibility=View.GONE
+        }
+
+
 
 
     }
